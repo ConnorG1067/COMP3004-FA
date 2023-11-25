d7909809788b3794e2d6fdb1d539454de11ea2ea
ConnorG1067 <77849049+ConnorG1067@users.noreply.github.com>
2023-11-25 14:30:22 -0500

    Headers commented

--- COMP3004-FA/AED.h
+++ COMP3004-FA/AED.h
@@ -11,34 +11,42 @@
 #include "AdultElectrode.h"
 #include "ChildElectrode.h"
 
-
+// AED inherits from QObject to send signals to mainwindow.h
 class AED : public QObject {
     Q_OBJECT;
 
+    // Private member variables
     private:
+        // Custom classes
         ElectrodePadPair* electrodePads;
         Victim* victim;
         VoiceSystem* voiceSystem;
 
+        // Ints
         int batteryLevel = 100;
+
+        // Bools
         bool isOn = false;
         bool correctPadPos = false;
         bool isFunctional = true;
         bool faultyPadPlacement = false;
         bool readyForShock = false;
         bool shockAdministered = false;
-
+    // Public member variables
     public:
+        // explicit constructor
         explicit AED();
+
+        // Shock btn functionality
         void shock();
 
+        // Power on the aed
         bool powerOn();
-        void monitorLoop();
 
         // Self Check
         bool selfCheck();
 
-        // Getters & Setters
+        // Getters
         bool getIsOn() { return this->isOn; }
         bool getIsReadyForShock() { return this->readyForShock; }
         int getBatteryLevel() { return this->batteryLevel; }
@@ -47,12 +55,11 @@ class AED : public QObject {
         bool getFaultyPadPlacment() { return this->faultyPadPlacement; }
         ElectrodePadPair* getElectrodePadPair() { return this->electrodePads; }
         bool getShockAdministered() { return this->shockAdministered; }
-
         Victim* getVictim() { return this->victim; }
         VoiceSystem* getVoiceSystem() { return this->voiceSystem; }
 
+        // Setters
         void setIsReadyForShock(bool isReady);
-
         void setBatteryLevel(int newBatteryLevel) { this->batteryLevel = newBatteryLevel; }
         void setIsOn(bool newIsOn) { this->isOn = newIsOn; }
         void setCorrectPadPos(bool padPositioning) { this->correctPadPos = padPositioning; }
@@ -61,7 +68,7 @@ class AED : public QObject {
         void setVictim(Victim *newVictim) { this->victim = newVictim; };
         void setElectrodePadPair(ElectrodePadPair* newPair) { this->electrodePads = newPair; }
         void setShockAdministered(bool newShockAdministered) { this->shockAdministered = newShockAdministered; }
-
+    // Signals for the aed
     signals:
         void arythmiaDetected();
         void flashShockButtonSignal();
--- COMP3004-FA/NormalSinusRhythm.h
+++ COMP3004-FA/NormalSinusRhythm.h
@@ -4,12 +4,17 @@
 #include <QString>
 #include "CardiacArrhythmias.h"
 
+// NormalSinusRhythm class inherits from CardiacArrhythmias
 class NormalSinusRhythm : public CardiacArrhythmias {
+    // Public functions
     public:
+        // Empty constructor
         NormalSinusRhythm(){};
-
+        // Treatment process override
         void treatmentProcedure() override {};
+        // Getter override
         QString getConditionName() override { return this->conditionName; }
+    // Private member variables
     private:
         QString conditionName = "Normal Sinus Rhythm";
 };
