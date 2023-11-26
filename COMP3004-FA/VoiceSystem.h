e5978d35bb2074581c079e463879087b5b20d812
ConnorG1067 <77849049+ConnorG1067@users.noreply.github.com>
2023-11-26 11:00:01 -0500

    Changed electrode btn to toggle

--- COMP3004-FA/VoiceSystem.h
+++ COMP3004-FA/VoiceSystem.h
@@ -8,30 +8,40 @@
 // In VoiceSystem.h
 class AED; // Forward declaration
 
+// Voice system class
 class VoiceSystem : public QObject {
     Q_OBJECT;
 
+    // Private member variables
     private:
+        // Text Instructions
         QString currentInstruction;
+        // Illustration Path
         QString currentIllustrationPath;
+        // AED assoicated with the voice system
         AED *aed;
+        // Media player for the VoiceSystem
         QMediaPlayer* audioInstructions;
-
+    // Public functions
     public:
+        // Constructors
         explicit VoiceSystem();
         explicit VoiceSystem(QObject*);
 
-        QString getCurrentInstruction();
-        void setCurrentInstruction(QString);
-
-        QString getCurrentIllustrationPath();
-        void setCurrentIllustrationPath(QString);
-
+        // Getters
+        QString getCurrentInstruction() { return this->currentInstruction; };
+        QString getCurrentIllustrationPath() { return this->currentIllustrationPath; };
         QMediaPlayer* getAudioInstructions() { return this->audioInstructions; }
 
-        // Message functions
+        // Setters
+        void setCurrentInstruction(QString);
+        void setCurrentIllustrationPath(QString);
+
+
+        // Message function
         void initiateAudioAndTextIntruction(QString, QString, QString);
 
+    // Signals
     signals:
         void textInstructionUpdatedForDisplay();
 };
--- COMP3004-FA/mainwindow.h
+++ COMP3004-FA/mainwindow.h
@@ -29,52 +29,58 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+    // Public functions
+    public:
+        // Constructor and Deconstructor
+        MainWindow(QWidget *parent = nullptr);
+        ~MainWindow();
+    // Private member variables
+    private:
+        // UI pointer
+        Ui::MainWindow *ui;
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+        // QGraphicsScenes
+        QGraphicsScene* instructionScene;
+        QGraphicsScene* imageInstructionScene;
+        QGraphicsScene* waveFormScene;
 
-private:
-    Ui::MainWindow *ui;
-    QGraphicsScene* instructionScene;
-    QGraphicsScene* imageInstructionScene;
-    QGraphicsScene* waveFormScene;
+        // AED for the MainWindow
+        AED* aed;
 
-    AED* aed;
+        // Timers
+        QTimer* flashTimer;
 
-    // Timers
-    QTimer* flashTimer;
+        // QRadioButton StyleSheets
+        static QString yellowRBIndicator;
+        static QString greenRBIndicator;
+        static QString redRBIndicator;
+        static QString blackUnfilledRBIndicator;
 
-    // QRadioButton Colors
-    static QString yellowRBIndicator;
-    static QString greenRBIndicator;
-    static QString redRBIndicator;
-    static QString blackUnfilledRBIndicator;
+        // function to setup connections-
+        void initializeBtns();
+        void initializeStartingUI();
+        void selfCheckUI(bool);
+        void displayDummy();
+        void callIndicatorSwitchLambdas();
 
-    // function to setup connections-
-    void initializeBtns();
-    void initializeStartingUI();
-    void selfCheckUI(bool);
-    void displayDummy();
-    void callIndicatorSwitchLambdas();
+        // Helpers
+        void resetRadioBtns();
+        void placeImage(QGraphicsScene*, QString, int, int, int, int);
+        QString determineCondition();
+        CardiacArrhythmias* imgPathToCardiac(QString);
+        void updateVictimInfo();
+        void placePadsUI(bool);
+        ElectrodePadPair* generateElectrodePadPair(bool);
+        void indiciatorSwitch(QRadioButton*, std::function<void()>, std::function<void()>, bool);
+        void placeAdultElectrodeBtn();
+        void placeChildElectrodeBtn();
 
-    // Helpers
-    void resetRadioBtns();
-    void placeImage(QGraphicsScene*, QString, int, int, int, int);
-    QString determineCondition();
-    CardiacArrhythmias* imgPathToCardiac(QString);
-    void updateVictimInfo();
-    void placePadsUI(bool);
-    ElectrodePadPair* generateElectrodePadPair(bool);
-    void indiciatorSwitch(QRadioButton*, std::function<void()>, std::function<void()>, bool);
-
-private slots:  
-    void powerBtn();
-    void failAEDSetupBtn();
-    void displayTextInstruction(QString);
-    void placeAdultElectrodeBtn();
-    void placeChildElectrodeBtn();
-    void flashShockButton();
-
+    // Private slots for the btns
+    private slots:
+        void powerBtn();
+        void failAEDSetupBtn();
+        void displayTextInstruction(QString);
+        void flashShockButton();
+        void determineElectrodes();
 };
 #endif // MAINWINDOW_H
